9452e345e40bcb53e44b5f81e1601ac138e66189
Michael Flickinger <Mike.A.Flickinger@gmail.com>
2019-09-27 16:19:35 -0400

    Saving dropdown changes

--- client/src/app/leagues/leagues.component.ts
+++ client/src/app/leagues/leagues.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RegionService } from './../providers/regions.service';
 import { LeagueService } from './../providers/leagues.service';
+import { TeamService } from './../providers/teams.service';
 
 @Component({
   selector: 'app-leagues',
@@ -11,25 +12,34 @@ export class LeaguesComponent implements OnInit {
 
   regions: Array<any> = [];
   leagues: Array<any> = [];
+  teams: Array<any> = [];
   selected;
     constructor(
       private regionService: RegionService,
       private leaguesService: LeagueService,
+      private teamService: TeamService,
       ) {}
 
   ngOnInit() {
-    this.regionService.getRegion().subscribe(data => {
-      console.log("data");
-      console.log(data[0].Name);
+    this.regionService.getRegion().subscribe(region => {
+      console.log("region");
+      console.log(region);
       console.log("~~~~~~~~~~~")
-      this.regions = data;
+      this.regions = region;
     });
 
-    this.leaguesService.getLeague().subscribe(data => {
-      console.log("data");
-      console.log(data);
+    this.leaguesService.getLeague().subscribe(league => {
+      console.log("league");
+      console.log(league);
       console.log("~~~~~~~~~~~")
-      this.regions = data;
+      this.leagues = league;
+    });
+
+    this.teamService.getTeam().subscribe(teams => {
+      console.log("teams");
+      console.log(teams);
+      console.log("~~~~~~~~~~~")
+      this.teams = teams;
     });
   }
 
