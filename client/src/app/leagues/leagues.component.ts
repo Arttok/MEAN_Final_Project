28ad8f1ec6c645a2cc73277797fa1bd12d1c04a2
Michael Flickinger <Mike.A.Flickinger@gmail.com>
2019-09-27 15:48:18 -0400

    Dropdown are populating correctly!

--- client/src/app/leagues/leagues.component.ts
+++ client/src/app/leagues/leagues.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { RegionService } from './../providers/regions.service';
+import { LeagueService } from './../providers/leagues.service';
 
 @Component({
   selector: 'app-leagues',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LeaguesComponent implements OnInit {
 
-  constructor() { }
+  regions: Array<any> = [];
+  leagues: Array<any> = [];
+  selected;
+    constructor(
+      private regionService: RegionService,
+      private leaguesService: LeagueService,
+      ) {}
 
   ngOnInit() {
+    this.regionService.getRegion().subscribe(data => {
+      console.log("data");
+      console.log(data[0].Name);
+      console.log("~~~~~~~~~~~")
+      this.regions = data;
+    });
+
+    this.leaguesService.getLeague().subscribe(data => {
+      console.log("data");
+      console.log(data);
+      console.log("~~~~~~~~~~~")
+      this.regions = data;
+    });
   }
 
 }
