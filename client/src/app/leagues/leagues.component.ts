d1cedd7fda8ef66bca761d204450b39c5d6bcfe9
Michael Flickinger <Mike.A.Flickinger@gmail.com>
2019-09-27 08:55:56 -0400

    update and leagues page added

---
+++ client/src/app/leagues/leagues.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-leagues',
+  templateUrl: './leagues.component.html',
+  styleUrls: ['./leagues.component.css']
+})
+export class LeaguesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
